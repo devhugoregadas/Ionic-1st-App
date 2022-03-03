f0307f4cbf3bc5971dbf47c049491be90ffc74f7
Hugo Regadas <dev.hugoregadas@gmail.com>
2022-03-03 16:24:45 +0100

    First commit

--- capacitor.config.ts
+++ capacitor.config.ts
@@ -1,7 +1,7 @@
 import { CapacitorConfig } from '@capacitor/cli';
 
 const config: CapacitorConfig = {
-  appId: 'io.ionic.starter',
+  appId: 'io.ionic.starter', //Domain name & App identifier
   appName: 'Ionic-1st-App',
   webDir: 'www',
   bundledWebRuntime: false
